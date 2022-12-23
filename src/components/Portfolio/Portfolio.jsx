6f0b8ab48840048d0ebb0309c7839535948f4002
Ranjeet Kumar <53788419+ranjeet9750@users.noreply.github.com>
2022-12-23 15:06:39 +0530

    portfolio update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,29 +1,84 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "This is my project title",
+    github: "https://github.com/ranjeet9750",
+    demo: "https://github.com/ranjeet9750"
+  }
+];
 
 const Portfolio = () => {
-  return(
+  return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'>
-            <img src={IMG1} alt=""/>
-          </div>
-          <h3>This is porfolio item title</h3>
-          <a href="" className='btn'>Github</a>
-          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
-        </article>
+      <div className="container portfolio_container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio_item">
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
-    
     </section>
-  )};
+  );
+};
 export default Portfolio;
